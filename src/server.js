3b9190b9cffcbc0d0104121afa9ead324b9ba006
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2023-01-19 14:43:23 -0400

    Headers - Usuários salvos em memória

--- src/server.js
+++ src/server.js
@@ -3,7 +3,19 @@ import http from 'node:http'
 // CommonJS => const givenModuleName = require('moduleName)
 // ESModules => import { givenModuleName } from 'moduleName'
 // Para usar o ESModules, ir no package.json e adicionar a propriedade "type": "module"
-// Para utilizar módulos próprios do Node, utiliza a sintaxe da importação igual do http acima
+// Para utilizar módulos próprios do Node, utilize a sintaxe da importação como do http acima ('node:${módulo}')
+
+// Stateful != Stateless
+// Stateful sempre tem uma informação sendo guardada em memória e depende dessa memória para que ela continue funionando
+// Stateless não salva as informações em memória. Geralmente armazena em dispositivos externos. Portanto ela não depende da memória e pode ser reiniciada e irá manter o comportamento normal.
+// Primeiramente iremos trabalhar com uma aplicação STATEFUL.
+
+const users = []
+
+// A resposta dada ao Array não pode ser um Array, portanto iremos utilizar JSON - JavaScript Object Notation (string). Muito utilizado na comunicação back-end <-> front-end & back-end <-> back-end
+
+// Headers (requisição e resposta) => METADADOS para que tanto o FE quanto o BE saibam lidar com determinada requisição ou resposta. Documentação: (https://developer.mozilla.org/en-US/docs/Web/API/Headers)
+// Podem ser utilizados back-end <-> front-end.
 
 // Rotas HTTP no servidor são caminhos de entrada na aplicação para receber requisições e enviar respostas.
 // Rota HTTP = API
@@ -26,10 +38,18 @@ const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader('Content-type', 'aplication/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'João',
+      email: 'jpedronogueira.dev@gmail.com'
+    })
+
     return res.end('Criação de usuários')
   }
 
