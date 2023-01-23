8b7fbc05be763dac671e6dfbd1137813b4d23edd
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2023-01-23 14:20:29 -0400

    Deleção de usuário utilizando RegEx

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
-import { Database } from './database.js'
 import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
 
@@ -27,9 +28,18 @@ export const routes = [
 
       database.insert('users', user)
 
-      return res
-        .writeHead(201) //201 -> New ressourse created successfully
-        .end() // Não presisa de nenhuma informação adicional
+      return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
     }
   }
 ]
